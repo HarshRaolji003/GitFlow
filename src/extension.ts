e24167a13072a26f3b968ca39379ca7a92ce7c34
Harsh Raolji <148987860+HarshRaolji003@users.noreply.github.com>
2025-11-26 22:46:09 +0530

    feat: commit graph in output (#1)

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,59 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { exec } from "child_process";
+import { promisify } from "util";
+import * as vscode from "vscode";
+
+const execAsync = promisify(exec);
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log("GitFlow extension activated!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gitflow" is now active!');
+  // Command 1: Hello World (KEEP THIS)
+  const helloWorldCmd = vscode.commands.registerCommand(
+    "gitflow.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from GitFlow!");
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('gitflow.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from GitFlow!');
-	});
+  // Command 2: Commit visualizer
+  const showCommitsCmd = vscode.commands.registerCommand(
+    "gitflow.showCommits",
+    showCommitHistory
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(helloWorldCmd, showCommitsCmd);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function showCommitHistory() {
+  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+
+  if (!workspaceFolder) {
+    vscode.window.showErrorMessage("Open a workspace folder first.");
+    return;
+  }
+
+  const cwd = workspaceFolder.uri.fsPath;
+
+  try {
+    // Git command: last 20 commits with branch graph
+    const { stdout } = await execAsync(
+      "git log --oneline --graph --decorate -n 20",
+      { cwd }
+    );
+
+    const output = vscode.window.createOutputChannel("GitFlow Commits");
+    output.clear();
+    output.appendLine("=== GitFlow: Commit History ===\n");
+    output.appendLine(stdout);
+    output.show(true);
+  } catch (error: any) {
+    vscode.window.showErrorMessage("GitFlow Error: " + error.message);
+  }
+}
